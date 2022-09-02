b5608fefb994120efea905d8795cb53ddf0c6ee8
ferdiand <ferdiand@gmail.com>
2022-09-02 20:57:25 +0000

    created first version

---
+++ custom_components/integration_victron/__init__.py
@@ -0,0 +1,27 @@
+"""
+Custom integration to integrate integration_blueprint with Home Assistant.
+
+For more details about this integration, please refer to
+https://github.com/custom-components/integration_blueprint
+"""
+
+from homeassistant.core import Config, HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+from .const import (
+    CONF_PASSWORD,
+    CONF_USERNAME,
+    DOMAIN,
+    PLATFORMS,
+    STARTUP_MESSAGE,
+)
+
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Your controller/hub specific code."""
+    # Data that you want to share with your platforms
+    hass.data[DOMAIN] = {"temperature": 23}
+
+    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
+
+    return True
