8f01acecc098c8161c74d71c032b11ecd4d45c7c
ferdiand <ferdiand@gmail.com>
2022-09-06 21:54:50 +0000

    little bug

--- custom_components/integration_victron/sensor.py
+++ custom_components/integration_victron/sensor.py
@@ -42,7 +42,7 @@ class VictronSensor(IntegrationVictronEntity, SensorEntity):
 
     def __init__(self, coordinator, config_entry, name, key):
         super().__init__(coordinator, config_entry, key)
-        self.coordinator._data[key] = 0.0
+        self.coordinator._data[key] = "0.0"
         self._attr_name = name
 
     @property
